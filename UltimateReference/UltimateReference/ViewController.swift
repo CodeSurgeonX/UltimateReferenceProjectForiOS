836e1e92e41d06d0a33273e27c9978fc007b85bc
Shashwat Kashyap <shashwat.kashyap@olx.com>
2021-01-15 16:30:26 +0530

    Basic Setup

---
+++ UltimateReference/UltimateReference/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  UltimateReference
+//
+//  Created by Shashwat Kashyap on 15/01/21.
+//  Copyright © 2021 Shashwat. All rights reserved.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+    }
+
+
+
+}
